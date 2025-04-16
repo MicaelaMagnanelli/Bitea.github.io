32e39b2f9f991e84bdc01eead5c18f6d7d1a7377
MicaelaMagnanelli <mica_magna@hotmail.com>
2025-04-15 23:18:20 -0300

    Add files via upload

---
+++ js/translation.js
@@ -0,0 +1,190 @@
+const translations = {
+    es: {
+        "menu-link": "Menú",
+        "comprar-link": "Comprar",
+        "personalizar-link": "Personalizar",
+        "buy-hero-btn": "Comprar",
+        "flavor-week": "Semana del 8 al 31 de Oct",
+        "our-bites": "Nuestros Bites",
+        "apple-bite-title": "Apple Bite",
+        "apple-bite-desc": "Un clásico reinventado: suave, dulce y lleno de sabor. Caramelo brillante y una base esponjosa que nunca falla.",
+        "lemon-bite-title": "Lemon Bite",
+        "lemon-bite-desc": "Dulce, ácido y chispeante, la frescura que te sacude. Cada bocado es un rayo de sol con crema de limón y merengue.",
+        "choco-bite-title": "Choco Bite",
+        "choco-bite-desc": "Un esponjoso cupcake de chocolate relleno con suave crema de cacao. Coronado con más chocolate y una lluvia de chips de frambuesa para los más golosos.",
+        "cookie-blast-title": "Cookie Blast",
+        "cookie-blast-desc": "Cupcake con cookies, cubierto con una cremosa capa de frosting de queso y caramelo, trocitos de galleta y un toque de caramelo...",
+        "cono-cup-title": "Cono Cup",
+        "cono-cup-desc": "Un esponjoso cupcake de cono cubierto con un relleno cremoso de dulce de leche, decorado con un toque de chocolate con leche.",
+        "maice-boom-title": "Maice Boom",
+        "maice-boom-desc": "Suave cupcake de alfajor de maicena, relleno con cremoso dulce de leche, decorado con crujientes trozos de maicena.",
+        "ice-bitea-dream-title": "Ice Bitea Dream",
+        "ice-bitea-dream-desc": "Relleno con un cremoso corazón de helado de fresa y decorado con coloridas chispas. ¡Un bocado que te transportará a un mundo de sueños helados y momentos felices!",
+        "pistachio-lover-title": "Pistacho Lover",
+        "pistachio-lover-desc": "Con una base suave de pistacho, relleno de nuez y glaseado de mantequilla de pistacho. Cada bocado es una explosión de sabor que hará que tus sentidos se deleiten.",
+        "banana-bliss-title": "Banana Bliss",
+        "banana-bliss-desc": "Descubre el placer tropical con nuestro cupcake de banana. Cada bocado es una deliciosa explosión de sabor que te transportará a un paraíso de frutas. ¡La combinación perfecta de dulzura y suavidad en cada mordisco!",
+        "flynn-bite-title": "Flynn Bite",
+        "flynn-bite-desc": "Cada bocado es una explosión de dulzura esponjosa y chocolate que te hará sentir como en tu niñez. ¡Puro placer irresistible!",
+        "oreo-bite-title": "Oreo Bite",
+        "oreo-bite-desc": "¡La combinación definitiva para los amantes de las galletas! Decorado con una mini Oreo crujiente, cada bocado es un viaje al paraíso de las galletas y la crema.",
+        "pretzel-crunch-title": "Pretzel Crunch",
+        "pretzel-crunch-desc": "Una mezcla perfecta de dulce y salado. Relleno con una suave crema de caramelo y decorado con crujientes trocitos de pretzel. Cada mordisco combina el contraste irresistible del caramelo dulce con el toque salado del pretzel.",
+        "cereal-bite-title": "Cereal Bite",
+        "cereal-bite-desc": "¡Despierta tu lado más divertido! Esponjoso y lleno de color. ¡Dulce, crujiente y totalmente irresistible!",
+        "details-btn-1": "Detalles",
+        "details-btn-2": "Detalles",
+        "details-btn-3": "Detalles",
+        "details-btn-4": "Detalles",
+        "details-btn-5": "Detalles",
+        "details-btn-6": "Detalles",
+        "details-btn-7": "Detalles",
+        "details-btn-8": "Detalles",
+        "details-btn-9": "Detalles",
+        "details-btn-10": "Detalles",
+        "details-btn-11": "Detalles",
+        "details-btn-12": "Detalles",
+        "details-btn-13": "Detalles",
+        "buy-btn-1": "Comprar",
+        "buy-btn-2": "Comprar",
+        "buy-btn-3": "Comprar",
+        "buy-btn-4": "Comprar",
+        "buy-btn-5": "Comprar",
+        "buy-btn-6": "Comprar",
+        "buy-btn-7": "Comprar",
+        "buy-btn-8": "Comprar",
+        "buy-btn-9": "Comprar",
+        "buy-btn-10": "Comprar",
+        "buy-btn-11": "Comprar",
+        "buy-btn-12": "Comprar",
+        "buy-btn-13": "Comprar",
+        "customize-title": "Tu bitea favorito",
+        "customize-desc": "Elige cómo quieres disfrutar de nuestros deliciosos cupcakes. ¡Ármalo a tu gusto! Mezcla masas, sabores y rellenos para crear la combinación perfecta.",
+        "customize-btn": "Personalizar",
+        "popup-title": "Armá tu cupcake",
+        "subtitulo-popup": "Elegí tu Frosting",
+        "continue-btn": "Continuar",
+        "healthy-options": "Opciones saludables",
+        "keto-title": "Keto",
+        "keto-desc": "Explora nuestras irresistibles opciones de sabores para dietas Keto y gluten free. Disfruta de cupcakes saludables y deliciosos, disponibles en una variedad de sabores que te sorprenderán. ¡El placer de un postre sin culpa!",
+        "btnKeto": "Keto",
+        "btnGlutenFree": "Gluten Free",
+        "checkout-btn": "Finalizar compra",
+        "footer-location": "Ubicación",
+        "footer-history": "Historia",
+        "footer-contact": "Contacto"
+    },
+    en: {
+        "menu-link": "Menu",
+        "comprar-link": "Buy",
+        "personalizar-link": "Customize",
+        "buy-hero-btn": "Buy",
+        "flavor-week": "Week of October 8th to 31st",
+        "our-bites": "Our Bites",
+        "apple-bite-title": "Apple Bite",
+        "apple-bite-desc": "A reinvented classic: soft, sweet, and full of flavor. Shiny caramel and a fluffy base that never fails.",
+        "lemon-bite-title": "Lemon Bite",
+        "lemon-bite-desc": "Sweet, tart, and sparkling, the freshness that shakes you up. Each bite is a ray of sunshine with lemon cream and meringue.",
+        "choco-bite-title": "Choco Bite",
+        "choco-bite-desc": "A fluffy chocolate cupcake filled with smooth cocoa cream. Topped with more chocolate and a shower of raspberry chips for the sweetest cravings.",
+        "cookie-blast-title": "Cookie Blast",
+        "cookie-blast-desc": "Cupcake with cookies, covered with a creamy layer of cheese and caramel frosting, cookie chunks, and a touch of caramel...",
+        "cono-cup-title": "Cone Cup",
+        "cono-cup-desc": "A fluffy cone cupcake covered with a creamy dulce de leche filling, decorated with a touch of milk chocolate.",
+        "maice-boom-title": "Maice Boom",
+        "maice-boom-desc": "Soft corn alfajor cupcake, filled with creamy dulce de leche, decorated with crunchy corn pieces.",
+        "ice-bitea-dream-title": "Ice Bitea Dream",
+        "ice-bitea-dream-desc": "Filled with a creamy strawberry ice cream core and decorated with colorful sprinkles. A bite that will transport you to a world of icy dreams and happy moments!",
+        "pistachio-lover-title": "Pistachio Lover",
+        "pistachio-lover-desc": "With a soft pistachio base, filled with nuts and glazed with pistachio butter. Each bite is a flavor explosion that will delight your senses.",
+        "banana-bliss-title": "Banana Bliss",
+        "banana-bliss-desc": "Discover tropical bliss with our banana cupcake. Each bite is a delicious flavor explosion that will transport you to a fruit paradise. The perfect combination of sweetness and softness in every bite!",
+        "flynn-bite-title": "Flynn Bite",
+        "flynn-bite-desc": "Each bite is an explosion of fluffy sweetness and chocolate that will make you feel like a kid again. Pure irresistible pleasure!",
+        "oreo-bite-title": "Oreo Bite",
+        "oreo-bite-desc": "The ultimate combination for cookie lovers! Decorated with a crunchy mini Oreo, each bite is a journey to cookie and cream paradise.",
+        "pretzel-crunch-title": "Pretzel Crunch",
+        "pretzel-crunch-desc": "A perfect blend of sweet and salty. Filled with a smooth caramel cream and decorated with crunchy pretzel pieces. Each bite combines the irresistible contrast of sweet caramel with the salty touch of pretzel.",
+        "cereal-bite-title": "Cereal Bite",
+        "cereal-bite-desc": "Awaken your fun side! Fluffy and full of color. Sweet, crunchy, and totally irresistible!",
+        "details-btn-1": "Details",
+        "details-btn-2": "Details",
+        "details-btn-3": "Details",
+        "details-btn-4": "Details",
+        "details-btn-5": "Details",
+        "details-btn-6": "Details",
+        "details-btn-7": "Details",
+        "details-btn-8": "Details",
+        "details-btn-9": "Details",
+        "details-btn-10": "Details",
+        "details-btn-11": "Details",
+        "details-btn-12": "Details",
+        "details-btn-13": "Details",
+        "buy-btn-1": "Buy",
+        "buy-btn-2": "Buy",
+        "buy-btn-3": "Buy",
+        "buy-btn-4": "Buy",
+        "buy-btn-5": "Buy",
+        "buy-btn-6": "Buy",
+        "buy-btn-7": "Buy",
+        "buy-btn-8": "Buy",
+        "buy-btn-9": "Buy",
+        "buy-btn-10": "Buy",
+        "buy-btn-11": "Buy",
+        "buy-btn-12": "Buy",
+        "buy-btn-13": "Buy",
+        "customize-title": "Your Favorite Bitea",
+        "customize-desc": "Choose how you want to enjoy our delicious cupcakes. Customize it to your liking! Mix doughs, flavors, and fillings to create the perfect combination.",
+        "customize-btn": "Customize",
+        "popup-title": "Build Your Cupcake",
+        "subtitulo-popup": "Choose Your Frosting",
+        "continue-btn": "Continue",
+        "healthy-options": "Healthy Options",
+        "keto-title": "Keto",
+        "keto-desc": "Explore our irresistible flavor options for Keto and gluten-free diets. Enjoy healthy and delicious cupcakes, available in a variety of surprising flavors. The pleasure of a guilt-free dessert!",
+        "btnKeto": "Keto",
+        "btnGlutenFree": "Gluten Free",
+        "checkout-btn": "Checkout",
+        "footer-location": "Location",
+        "footer-history": "History",
+        "footer-contact": "Contact"
+    }
+};
+
+// Función para obtener el parámetro 'lang' de la URL
+function getLanguageFromURL() {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get('lang') || null; // Devuelve null si no hay parámetro
+}
+
+// Función para actualizar el contenido según el idioma
+function updateLanguage() {
+    // Obtener el idioma de la URL o de localStorage
+    let language = getLanguageFromURL();
+    
+    // Si no hay idioma en la URL, intentar obtenerlo de localStorage
+    if (!language) {
+        language = localStorage.getItem('language') || 'es';
+    } else {
+        // Guardar el idioma en localStorage para futuras visitas
+        localStorage.setItem('language', language);
+    }
+
+    // Asegurarse de que el idioma sea válido ('es' o 'en')
+    language = ['es', 'en'].includes(language) ? language : 'es';
+
+    // Actualizar el contenido de la página
+    const texts = translations[language] || translations['es'];
+    Object.keys(texts).forEach(id => {
+        const element = document.getElementById(id);
+        if (element) {
+            element.textContent = texts[id];
+        }
+    });
+
+    // Actualizar el atributo lang del HTML para accesibilidad
+    document.documentElement.lang = language;
+}
+
+// Ejecutar la función al cargar la página
+window.addEventListener('DOMContentLoaded', updateLanguage);
